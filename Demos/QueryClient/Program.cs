0752e7d2888ba3e9f430a3632e98e7e51c91247d
Paul Glavich <glav@theglavs.com>
2018-08-23 15:12:10 +1000

    Added a simple query demo and renamed soln

---
+++ Demos/QueryClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using CosmosCommon.Config;
+using System;
+
+namespace QueryClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cosmosConfig = AppConfig.GetCosmosConfig();
+            int iterationCount;
+
+            if (args.Length == 0 || !int.TryParse(args[0],out iterationCount))
+            {
+                iterationCount = 1;
+            }
+
+            Console.WriteLine($"Running query {iterationCount} times...");
+
+            var client = new QueryRunner(cosmosConfig);
+            var results = client.SimpleQueryAsync().Result;
+
+        }
+    }
+}
---
+++ Demos/QueryClient/QueryRunner.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using System.Linq;
+using CosmosCommon.Helpers;
+using CosmosCommon.CosmosDb;
+using Microsoft.Azure.Documents.Linq;
+using System.Collections.Generic;
+
+namespace QueryClient
+{
+
+    public class QueryRunner
+    {
+        private readonly AsyncRunner asynRunner = new AsyncRunner();
+        private readonly DocumentClient documentClient;
+        private readonly CosmosDbConfig cosmosConfig;
+
+        public QueryRunner(CosmosDbConfig cosmosConfig)
+        {
+            this.cosmosConfig = cosmosConfig;
+
+            this.documentClient = new DocumentClient(new Uri(cosmosConfig.Endpoint), cosmosConfig.Key,
+                new ConnectionPolicy { ConnectionMode = ConnectionMode.Direct, ConnectionProtocol = Protocol.Tcp, MaxConnectionLimit = 1000 });
+            this.documentClient.OpenAsync();
+        }
+
+        public async Task<List<DataDocument>> SimpleQueryAsync()
+        {
+
+            var uri = UriFactory.CreateDocumentCollectionUri(this.cosmosConfig.DatabaseId, this.cosmosConfig.LargeCollectionId);
+            var results = new List<DataDocument>();
+
+            var query = this.documentClient.CreateDocumentQuery(uri,
+                "SELECT c.id,c.partitionKey FROM c where contains (c.appId,\"12\") ",
+                    new FeedOptions {
+                                EnableCrossPartitionQuery = true,
+                    //            MaxDegreeOfParallelism = -1
+                    }).AsDocumentQuery();
+
+            while (query.HasMoreResults)
+            {
+                Console.WriteLine(" > Getting next set of results");
+
+                var nextResults = await query.ExecuteNextAsync<DataDocument>();
+
+                Console.WriteLine(" > Retrieved {0} results", nextResults.Count);
+                results.AddRange(nextResults);
+            }
+            return results.ToList();
+        }
+    }
+}
